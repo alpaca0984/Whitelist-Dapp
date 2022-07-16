f279ca89b1dcaac70c416de028eda72f2fd720b1
alpaca0984 <alpaca0984@gmail.com>
2022-07-16 14:00:44 +0000

    fix: specify Goerli chain id

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
 
     // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
+    if (chainId !== 5) {
       window.alert("Change the network to Goerli");
       throw new Error("Change network to Goerli");
     }
